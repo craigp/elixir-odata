e045afb40221be63cfce115b2d019f1f3f92f3d1
Craig Paterson <craig@widdershins.co.za>
2017-06-26 15:51:34 +0200

    version bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule OData.Mixfile do
 
   def project do
     [app: :odata,
-     version: "0.1.6",
+     version: "0.1.7",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
