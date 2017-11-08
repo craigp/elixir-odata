2a01a0f70def3c2fbdff13a1725f0f639a288213
Craig Paterson <craig@widdershins.co.za>
2017-11-08 15:06:16 +0200

    deps update

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule OData.Mixfile do
 
   def project do
     [app: :odata,
-     version: "0.1.7",
+     version: "0.1.8",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
